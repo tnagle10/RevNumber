c22d9a6683f17e84cf24ffc1cb60cc219b87a5b7
tnagle10 <tnagle10@gmail.com>
2016-10-14 22:57:16 -0400

    Added ConvToDecimalWithErrorCheck and RevDec methods

--- Program.cs
+++ Program.cs
@@ -5,80 +5,123 @@ namespace RevNumber
 {
     class Program
     {
+
+
+
+        private static decimal ConvToDecimalWithErrorCheck(string Input)
+        {
+            // This method converts a string to decimal using Convert.ToDecimal().
+            // It uses System error checking to make sure the input valid.
+            // If the input is valid, it returns the input converted to decimal.
+            // If the input is invalid, it returns a 0  
+
+            var Space = "\n";
+            try
+            {
+                // Verify user input is in correct format 
+                Decimal Output = Convert.ToDecimal(Input);
+                return Output;
+            }
+            catch (System.OverflowException)
+            {
+                Console.WriteLine(Space);
+                System.Console.WriteLine(
+                    "The conversion from string to number overflowed.");
+                Console.WriteLine(Space);
+                return 0;
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine(Space);
+                System.Console.WriteLine(
+                    "The string is not formatted as a number.");
+                Console.WriteLine(Space);
+                return 0;
+            }
+            catch (System.ArgumentNullException)
+            {
+                Console.WriteLine(Space);
+                System.Console.WriteLine(
+                    "The string is null.");
+                Console.WriteLine(Space);
+                return 0;
+            }
+
+        }
+
+        private static decimal RevDec(decimal Input)
+        {
+            // This method takes a decimal as input, and if the input is a valid whole number.
+            // Print Returns the reverse of the number as output.
+            Decimal Divider = 10;
+            Decimal Remainder = 0;
+            String OutputString = "";
+            Decimal OutputNumber = 0;
+           
+
+            Decimal Quotient = Input;  // Set initial quotient to entire number entered
+
+            // While we are not at the end of the number (i.e. dividing by 10 doesn't give us a zero quotient).
+            // Create a string of remainders in reverse order
+            while (Quotient != 0)
+            {
+                // Calculate the quotient and remainder 
+                Remainder = Quotient % Divider;
+                Quotient = Math.Round(Quotient / Divider);
+                Convert.ToString(Remainder);
+                OutputString = OutputString + Remainder;
+            }
+
+            OutputNumber = ConvToDecimalWithErrorCheck(OutputString);
+            if (OutputNumber != 0)
+            {
+                return OutputNumber;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+
         static void Main(string[] args)
         {
-            string Line = "";
+            string InputLine = "";
+            decimal OutputNumber = 0;
+            
             var Space = "\n";
 
-            //Repeat this code until the user enters "y"
-            while (Line != "q")
+            //Repeat this code until the user enters "q"
+            while (InputLine != "q")
             {
                 // Get number from user
-                Console.WriteLine("This program will reverse a whole number you enter (No decimal points).  Please enter a number to begin. (Type y to exit)");
-                Line = Console.ReadLine(); 
+                Console.WriteLine("This program will reverse a whole number you enter (No decimal points).  Please enter a number to begin. (Type q to exit)");
+                InputLine = Console.ReadLine();
 
-                // If user enters "y", execute exit procedure
-                if (Line == "y") 
+                // If user enters "q", execute exit procedure
+                if (InputLine == "q")
                 {
                     Console.WriteLine(Space);
-                    Console.WriteLine("You entered y");
+                    Console.WriteLine("You entered q");
                     Console.WriteLine(Space);
-                    Console.WriteLine("Are you sure you want to exit? (Type y to confirm)"); //Make sure they want to quit
-                    Line = Console.ReadLine(); // Get string from user        
-                    if (Line == "y")
+                    Console.WriteLine("Are you sure you want to exit? (Type y to confirm)"); 
+                    InputLine = Console.ReadLine();        
+                    if (InputLine == "y")
                     { return; }
-
+                  
                 }
 
-                // Try to reverse number
-                Decimal Divider = 10; 
-                Decimal Remainder = 0;
-                String Output = "";
-                try
+                decimal InputNumber = ConvToDecimalWithErrorCheck(InputLine);
+                if (InputNumber != 0)
                 {
-                    // Verify user input is in correct format 
-                    Decimal InputNumber = Convert.ToDecimal(Line);
-                    Decimal Quotient = InputNumber;  // Set initial quotient to entire number entered
-
-                    // While we are not at the end of the number (i.e. dividing by 10 doesn't give us a zero quotient).
-                    // Create a string of remainders in reverse order
-                    while (Quotient != 0)
-                    {
-                        // Calculate the quotient and remainder 
-                        Remainder = Quotient % Divider;
-                        Quotient = Math.Round(Quotient / Divider);
-                        Convert.ToString(Remainder);
-                        Output = Output + Remainder;
-                    }
-
+                    OutputNumber = RevDec(InputNumber);
                     Console.WriteLine(Space);
                     Console.WriteLine("Here is the number reversed:");
-                    Console.WriteLine(Output);
+                    Console.WriteLine(OutputNumber);
                     Console.WriteLine(Space);
                 }
-                catch (System.OverflowException)
-                {
-                    Console.WriteLine(Space);
-                    System.Console.WriteLine(
-                        "The conversion from string to number overflowed.");
-                        Console.WriteLine(Space);
-                }
-                catch (System.FormatException)
-                {
-                    Console.WriteLine(Space);
-                    System.Console.WriteLine(
-                        "The string is not formatted as a number.");
-                        Console.WriteLine(Space);
-                }
-                catch (System.ArgumentNullException)
-                {
-                    Console.WriteLine(Space);
-                    System.Console.WriteLine(
-                        "The string is null.");
-                        Console.WriteLine(Space);
-                }
-
+               
             }
-         }
+        }
     }
 }
